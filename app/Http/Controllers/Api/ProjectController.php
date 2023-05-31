f08d74d5bface6db24741ae04b7e8e81386c8a49
martinapiglio <martina.piglio@gmail.com>
2023-05-31 14:18:38 +0200

    feat: route to single project for front-end view + show() method in Api/PorjectController added

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -18,4 +18,25 @@ class ProjectController extends Controller
         ]);
 
     }
+
+    public function show($slug) {
+        $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
+
+        if($project) {
+
+            return response()->json([
+                'success' => true,
+                'project' => $project,
+            ]);
+
+        } else {
+
+            return response()->json([
+                'success' => false,
+                'error' => 'Selected project does not exist',
+            ]);
+
+        }
+
+    }
 }
