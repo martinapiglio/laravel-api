37602cc3cbca09c76c02f927a85df30d38b91f2a
martinapiglio <martina.piglio@gmail.com>
2023-06-02 15:45:45 +0200

    feat: type request filter on projects receveid from front-end added in Api/ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,17 +4,37 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
 
         $projects = Project::with('type', 'technologies')->orderBy('projects.created_at', 'desc')->paginate(4);
+        $types = Type::all();
+
+        $formData = $request->all();
+        
+        if($request->has('type_id') && $formData['type_id'] != "") {
+
+            $projects = Project::where('type_id', $formData['type_id'])
+                                ->with('type', 'technologies')
+                                ->orderBy('projects.created_at', 'desc')
+                                ->paginate(4);
+
+            if(count($projects) == 0) {
+                return response()->json([
+                    'success' => false,
+                    'error' => 'No projects found in this category',
+                ]);
+            }
+        }
 
         return response()->json([
             'success' => true,
-            'results' => $projects
+            'results' => $projects,
+            'types' => $types,
         ]);
 
     }
